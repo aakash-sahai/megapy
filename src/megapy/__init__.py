12425622937debb5323486b703361cf77986a478
Aakash Sahai <sahaiaakash@gmail.com>
2018-10-19 21:37:23 -0600

    Several enhancements in handling device reconnection

--- src/megapy/__init__.py
+++ src/megapy/__init__.py
@@ -1,10 +1,11 @@
-from arduino import ArduinoConnection, ArduinoObject
+from arduino import ArduinoConnection, ArduinoObject, MegaException
 from pin import DigitalPin, AnalogPin
 from pushbutton import PushButton
 from stepper import Stepper
 
 __all__ = [ 'ArduinoConnection',
             'ArduinoObject',
+            'MegaException',
             'DigitalPin',
             'AnalogPin',
             'PushButton',
--- src/megapy/arduino.py
+++ src/megapy/arduino.py
@@ -1,25 +1,27 @@
 import serial
 import serial.tools.list_ports
 
+class MegaException(Exception):
+
+    def __init__(self, msg):
+        Exception.__init__(self, msg)
+
 class ArduinoConnection(object):
 
     def __init__(self, tty = None, baud = 115200):
-        if (tty is None):
-            self.select_tty()
-        else:
-            self.tty = tty
+        self.serial = None
+        self.tty = None
         self.baud = baud
-        self.serial = serial.Serial(self.tty, baud, timeout=1)
-        line = self.serial.readline()
-        if line.find('Ready') == -1:
-            self.flush()
-            self.ping()
+        if (tty is None):
+            tty = self.select_tty()
+        if tty is not None:
+            self.open(tty)
 
     def ping(self):
         self.serial.write('ping\n')
         line = self.serial.readline()
         if line.find('pong') == -1:
-            raise Exception('Expected pong, got: ' + line)
+            raise MegaException('Expected pong, got: ' + line)
 
     def flush(self):
         self.serial.write('\n')
@@ -31,28 +33,50 @@ class ArduinoConnection(object):
     def readline(self):
         return self.serial.readline()
 
-    def close(self):
-        self.serial.close()
+    def open(self, tty):
+        try:
+            if self.serial is not None:
+                self.serial.close()
+            self.tty = tty
+            self.serial = serial.Serial(self.tty, self.baud, timeout=1)
+            line = self.serial.readline()
+            if line.find('Ready') == -1:
+                self.flush()
+                self.ping()
+        except Exception as ex:
+            raise MegaException(str(ex))
 
-    def select_tty(self):
-        ports = list(serial.tools.list_ports.comports())
-        selected = None
-        print("Availble devices: [")
+    def close(self):
+        if self.serial is not None:
+            self.serial.close()
+            self.serial = None
+
+    @classmethod
+    def get_ttys(cls):
+        return list(serial.tools.list_ports.comports())
+
+    @classmethod
+    def print_ttys(cls):
+        ports = cls.get_ttys()
+        print("[")
         for p in ports:
-            fmt = '  {{ "name" : "{}" , "description" : "{}" , "manufacturer" : "{}" , "serial" : "{}" , "device" : "{}" }},'
-            print(fmt.format(p.name, p.description, p.manufacturer, p.serial_number, p.device))
-            if selected is None:
-                if p.description.find('Arduino') != -1 or \
-                  p.manufacturer.find('Arduino') != -1 or \
-                  p.manufacturer.find('FTDI') != -1:
-                    selected = p
+            try:
+                fmt = '  {{ "name" : "{}" , "description" : "{}" , "manufacturer" : "{}" , "serial" : "{}" , "device" : "{}" }},'
+                print(fmt.format(p.name, p.description, p.manufacturer, p.serial_number, p.device))
+            except:
+                pass
         print(']')
 
-        if selected:
-            print("Picking device: {} (serial: {})".format(selected.device, selected.serial_number))
-            self.tty = selected.device
-        else:
-            raise Exception("No connected Arduino devices found")
+    def select_tty(self):
+        for p in self.get_ttys():
+            try:
+                if p.description.find('Arduino') != -1 or \
+                   p.manufacturer.find('Arduino') != -1 or \
+                   p.manufacturer.find('FTDI') != -1:
+                        return p.device
+            except:
+                pass
+        return None
 
 
 class ArduinoObject(object):
@@ -64,31 +88,27 @@ class ArduinoObject(object):
 
     def do(self, args):
         cmd = 'do {} {}\n'.format(self.name, args)
-        #print cmd
         self.conn.write(cmd)
         line = self.conn.readline()
         if line.find('do ok') == -1:
-            raise Exception(line)
+            raise MegaException(line)
 
     def create(self, args):
         cmd = 'create {} as {} {}\n'.format(self.name, self.type, args)
-        #print cmd
         self.conn.write(cmd)
         line = self.conn.readline()
         if line.find('create ok') == -1:
-            raise Exception(line)
+            raise MegaException(line)
 
     def get(self, attr):
         cmd = 'get {} {}\n'.format(self.name, attr)
-        #print cmd
         self.conn.write(cmd)
         line = self.conn.readline()
         return line.rstrip()    # Trim trailing newline and other whitespaces
 
     def set(self, attr, value):
         cmd = 'set {} {} {}\n'.format(self.name, attr, value)
-        #print cmd
         self.conn.write(cmd)
         line = self.conn.readline()
         if line.find('set ok') == -1:
-            raise Exception(line)
+            raise MegaException(line)
